45574f662988fd84b1f2bb0f70feb65660913e70
songrong518 <songrong518@163.com>
2022-06-26 13:14:35 +0800

    测试连接是否正常

--- 算法题/暂停1s输出.py
+++ 算法题/暂停1s输出.py
@@ -1,8 +1,3 @@
 import time
 
-print(str(time.localtime()))
-time.sleep(1)
-
-for i in range(4):
-    print(time.strftime("%Y-%Y-%d: %H:%M:%S",time.localtime(time.time())))
-    time.sleep(1)
+print(time.time())
